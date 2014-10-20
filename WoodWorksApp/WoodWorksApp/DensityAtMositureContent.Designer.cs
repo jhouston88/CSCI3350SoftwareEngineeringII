639e2d917d02b92d9f2baf8cea37c99b0afd89e9
trhaochris <haot01@goldmail.etsu.edu>
2014-10-20 14:04:07 -0400

    Change the form name

--- WoodWorksApp/WoodWorksApp/DensityAtMositureContent.Designer.cs
+++ WoodWorksApp/WoodWorksApp/DensityAtMositureContent.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form2";
+            this.Text = "DensityAtMositureContentForm";
         }
 
         #endregion
