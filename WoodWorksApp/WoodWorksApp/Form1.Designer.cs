724df8999239311758cffd81e38b995622988286
mcmahonn <mcmahonn@goldmail.etsu.edu>
2014-10-26 16:35:36 -0400

    added panels, toyed with layout slighty.

--- WoodWorksApp/WoodWorksApp/Form1.Designer.cs
+++ WoodWorksApp/WoodWorksApp/Form1.Designer.cs
@@ -42,33 +42,42 @@
             this.calculateComboBox = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.categoryPanel = new System.Windows.Forms.Panel();
+            this.woodPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.categoryPanel.SuspendLayout();
+            this.woodPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // categoryListBox
             // 
+            this.categoryListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.categoryListBox.FormattingEnabled = true;
-            this.categoryListBox.Location = new System.Drawing.Point(12, 32);
+            this.categoryListBox.Location = new System.Drawing.Point(0, 16);
             this.categoryListBox.Name = "categoryListBox";
-            this.categoryListBox.Size = new System.Drawing.Size(179, 251);
+            this.categoryListBox.Size = new System.Drawing.Size(180, 251);
             this.categoryListBox.TabIndex = 0;
             this.categoryListBox.SelectedIndexChanged += new System.EventHandler(this.categoryListBox_SelectedIndexChanged);
             // 
             // speciesListBox
             // 
+            this.speciesListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.speciesListBox.FormattingEnabled = true;
-            this.speciesListBox.Location = new System.Drawing.Point(215, 32);
+            this.speciesListBox.Location = new System.Drawing.Point(0, 16);
             this.speciesListBox.Name = "speciesListBox";
-            this.speciesListBox.Size = new System.Drawing.Size(165, 251);
+            this.speciesListBox.Size = new System.Drawing.Size(180, 251);
             this.speciesListBox.TabIndex = 1;
             this.speciesListBox.SelectedIndexChanged += new System.EventHandler(this.speciesListBox_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 4;
@@ -77,7 +86,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(212, 16);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 5;
@@ -86,7 +96,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(404, 16);
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 6;
@@ -109,7 +120,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(598, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(582, 24);
             this.menuStrip1.TabIndex = 11;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -127,7 +138,8 @@
             // 
             // descripListBox
             // 
-            this.descripListBox.Location = new System.Drawing.Point(407, 32);
+            this.descripListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.descripListBox.Location = new System.Drawing.Point(0, 16);
             this.descripListBox.Multiline = true;
             this.descripListBox.Name = "descripListBox";
             this.descripListBox.ReadOnly = true;
@@ -137,17 +149,14 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.woodPanel);
+            this.panel1.Controls.Add(this.categoryPanel);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.descripListBox);
-            this.panel1.Controls.Add(this.categoryListBox);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.speciesListBox);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(598, 353);
+            this.panel1.Size = new System.Drawing.Size(582, 353);
             this.panel1.TabIndex = 13;
             // 
             // calculateComboBox
@@ -177,14 +186,41 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 289);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(598, 64);
+            this.panel2.Size = new System.Drawing.Size(582, 64);
             this.panel2.TabIndex = 13;
+            // 
+            // categoryPanel
+            // 
+            this.categoryPanel.Controls.Add(this.label1);
+            this.categoryPanel.Controls.Add(this.categoryListBox);
+            this.categoryPanel.Location = new System.Drawing.Point(12, 16);
+            this.categoryPanel.Name = "categoryPanel";
+            this.categoryPanel.Size = new System.Drawing.Size(180, 267);
+            this.categoryPanel.TabIndex = 11;
+            // 
+            // woodPanel
+            // 
+            this.woodPanel.Controls.Add(this.label2);
+            this.woodPanel.Controls.Add(this.speciesListBox);
+            this.woodPanel.Location = new System.Drawing.Point(203, 16);
+            this.woodPanel.Name = "woodPanel";
+            this.woodPanel.Size = new System.Drawing.Size(180, 267);
+            this.woodPanel.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.descripListBox);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(389, 16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(180, 267);
+            this.panel3.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 377);
+            this.ClientSize = new System.Drawing.Size(582, 377);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -195,9 +231,14 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.categoryPanel.ResumeLayout(false);
+            this.categoryPanel.PerformLayout();
+            this.woodPanel.ResumeLayout(false);
+            this.woodPanel.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,6 +260,9 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox calculateComboBox;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel woodPanel;
+        private System.Windows.Forms.Panel categoryPanel;
     }
 }
 
