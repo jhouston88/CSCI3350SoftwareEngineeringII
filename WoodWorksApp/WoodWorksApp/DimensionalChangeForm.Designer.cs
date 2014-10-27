09809b8be0c5d6b6d96fe66730b9e7b235f808f7
mcmahonn <mcmahonn@goldmail.etsu.edu>
2014-10-27 10:52:07 -0400

    Made dimensional change form functional

--- WoodWorksApp/WoodWorksApp/DimensionalChangeForm.Designer.cs
+++ WoodWorksApp/WoodWorksApp/DimensionalChangeForm.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.woodLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.radialRadioButton = new System.Windows.Forms.RadioButton();
+            this.tangentialradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // woodLabel
@@ -40,15 +49,100 @@
             this.woodLabel.TabIndex = 0;
             this.woodLabel.Text = "woodLabel";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width:";
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(57, 36);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.widthTextBox.TabIndex = 0;
+            this.widthTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "△D:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(59, 138);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(58, 162);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // radialRadioButton
+            // 
+            this.radialRadioButton.AutoSize = true;
+            this.radialRadioButton.Checked = true;
+            this.radialRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.radialRadioButton.Name = "radialRadioButton";
+            this.radialRadioButton.Size = new System.Drawing.Size(55, 17);
+            this.radialRadioButton.TabIndex = 5;
+            this.radialRadioButton.TabStop = true;
+            this.radialRadioButton.Text = "Radial";
+            this.radialRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // tangentialradioButton
+            // 
+            this.tangentialradioButton.AutoSize = true;
+            this.tangentialradioButton.Location = new System.Drawing.Point(6, 42);
+            this.tangentialradioButton.Name = "tangentialradioButton";
+            this.tangentialradioButton.Size = new System.Drawing.Size(75, 17);
+            this.tangentialradioButton.TabIndex = 6;
+            this.tangentialradioButton.Text = "Tangential";
+            this.tangentialradioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radialRadioButton);
+            this.groupBox1.Controls.Add(this.tangentialradioButton);
+            this.groupBox1.Location = new System.Drawing.Point(16, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 70);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grain Direction";
+            // 
             // DimensionalChangeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(182, 192);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.woodLabel);
             this.Name = "DimensionalChangeForm";
             this.Text = "DimensionalChangeForm";
             this.Load += new System.EventHandler(this.DimensionalChangeForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +151,13 @@
         #endregion
 
         private System.Windows.Forms.Label woodLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.RadioButton radialRadioButton;
+        private System.Windows.Forms.RadioButton tangentialradioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- WoodWorksApp/WoodWorksApp/DimensionalChangeForm.cs
+++ WoodWorksApp/WoodWorksApp/DimensionalChangeForm.cs
@@ -25,5 +25,23 @@ namespace WoodWorksApp
             woodLabel.Text = wood.TreeName + " " + wood.Category.CategoryName;
         }
 
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double width;
+            // if the radial button isn't checked, then the tangential button must be checked.
+            Wood.Direction direction = radialRadioButton.Checked ? Wood.Direction.RADIAL : Wood.Direction.TANGENTIAL;
+
+            // make sure the user inputted a valid width.
+            if (double.TryParse(widthTextBox.Text, out width) && width > 0)
+            {
+                resultTextBox.Text = wood.calculateDimensionalChange(width, direction).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Width must be a positive number");
+                resultTextBox.Text = String.Empty;
+            }
+        }
+
     }
 }
