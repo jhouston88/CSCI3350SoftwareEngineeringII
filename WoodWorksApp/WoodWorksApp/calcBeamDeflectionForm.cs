99ec41ef7642aba66319841761ea6c93c90bcad3
EricBlumenstock <ericblumenstock2@gmail.com>
2014-11-19 20:49:51 -0500

    Minor changes
    Added decimal format "0.###" and updated comments.

--- WoodWorksApp/WoodWorksApp/calcBeamDeflectionForm.cs
+++ WoodWorksApp/WoodWorksApp/calcBeamDeflectionForm.cs
@@ -3,7 +3,7 @@
  * 
  * It reads the type of wood that is passed from the startup form, 
  * initializes all of the fields in the form, and when the calculate
- * button is clicked it sets the values with the desired fields.
+ * button is clicked it sets the values with the corresponding fields.
  * 
  * Authors: Josh Houston, Nick McMahon, Eric Blumenstock, 
  * and Tianran Hao
@@ -50,6 +50,7 @@ namespace WoodWorksApp
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+            //These four parse the values then return true if parsing succeeded
             double width;
             bool widthCheck = double.TryParse(beamWidthEntry.Text, out width);
             
@@ -62,12 +63,14 @@ namespace WoodWorksApp
             double load;
             bool loadCheck = double.TryParse(beamLoadEntry.Text, out load);
 
+            //Grain type radio button
             bool grainIsFlat;
             if (radioFlat.Checked)
                 grainIsFlat = true;
             else
                 grainIsFlat = false;
 
+            //Moisture radio button
             bool elasticityIs12percent;
             if (radioTwelvePct.Checked)
                 elasticityIs12percent = true;
@@ -76,7 +79,7 @@ namespace WoodWorksApp
 
             //If all parsers returned true for no errors AND all variables are greater than zero ELSE display a message box
             if ((widthCheck && depthCheck && spanCheck && loadCheck) && (width > 0 && depth > 0 && load > 0 && span > 0))
-                resultDisplay.Text = wood.calculateBeamDeflection(width, depth, load, span, elasticityIs12percent, grainIsFlat).ToString();
+                resultDisplay.Text = wood.calculateBeamDeflection(width, depth, load, span, elasticityIs12percent, grainIsFlat).ToString("0.###");
             else
             {
                 MessageBox.Show("All entries must be positive numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -85,7 +88,7 @@ namespace WoodWorksApp
 
             if (resultDisplay.Text == "Infinity")
             {
-                MessageBox.Show("Wood type " + wood.TreeName + " has no beam shear for specified grain type and moisture content.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Wood species " + wood.TreeName + " has no beam shear for specified grain type and moisture content.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 resultDisplay.Text = String.Empty;
             }
         }
